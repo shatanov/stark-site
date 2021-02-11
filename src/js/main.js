bda29c7f55113354cd73e43da611107e8283557d
shatanov <timur-shatanov.work@mail.ru>
2021-02-11 20:28:14 +0300

    mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,13 @@ const mobileMenu = document.querySelector('.header__mobile--nav');
 const navLink = document.querySelectorAll('.mobile__nav__link');
 const infoSelect = document.querySelectorAll('.info__select');
 const selectContent = document.querySelectorAll('.select__content');
-const selectorMetal = document.querySelector('.metal')
+const selectorMetal = document.querySelector('.metal');
+const body = document.querySelector('body');
+const mobileNav = document.querySelector('.mobile__nav')
+const pageWidth = window.innerWidth;
+const pageHeight = window.innerHeight;
+const mediaWidth = window.matchMedia('(min-width: 990px)')
+const mediaHeight = window.matchMedia('(max-height: 570px)')
 
 
 const viewSelectContent = (index) => {
@@ -28,6 +34,21 @@ const viewSelectContent = (index) => {
   }
 }
 
+const weidthCheck = (e) => {
+  if (e.matches) {
+    menuBtn.classList.remove('open-menu')
+    mobileMenu.classList.remove('menu-active')
+    body.classList.remove('overflow-hidden')
+  }
+}
+
+const heightCheck = (e) => {
+  if (e.matches){
+    mobileNav.style.overflowY = 'auto'
+    mobileNav.style.height = '135vh'
+  }
+}
+
 const navMenuFunction = () => {
   menuBtn.classList.contains('open-menu')
     ? menuBtn.classList.remove('open-menu')
@@ -35,9 +56,10 @@ const navMenuFunction = () => {
   mobileMenu.classList.contains('menu-active')
     ? mobileMenu.classList.remove('menu-active')
     : mobileMenu.classList.add('menu-active');
-
+  body.classList.contains('overflow-hidden')
+    ? body.classList.remove('overflow-hidden')
+    : body.classList.add('overflow-hidden');
 }
-
 
 
 menuBtn.addEventListener('click', () => {
@@ -55,6 +77,12 @@ infoSelect.forEach((el, index) => {
   el.addEventListener('click', () => viewSelectContent(index))
 })
 
+
+mediaWidth.addListener(weidthCheck)
+weidthCheck(mediaWidth)
+
+mediaHeight.addListener(heightCheck)
+heightCheck(mediaHeight)
 
 
 
